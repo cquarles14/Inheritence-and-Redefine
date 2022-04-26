66ad08bae5df25741d4c15eb3e97661e3920553a
cquarles14 <catlynne.quarles@sinclair.edu>
2022-04-25 23:24:12 -0400

    Vehicle input/output programmed

--- Inheritence and Redefine/main.cpp
+++ Inheritence and Redefine/main.cpp
@@ -4,3 +4,28 @@
 // Inheritence and Redefine
 
 
+#include "SUV_C.hpp"
+
+int main()
+{
+    string manufacturer;
+    int yearBuilt;
+    int numDoors;
+    int gasCap;
+    Vehicle_C vehicle;
+    Car_C car;
+    SUV_C suv;
+    
+    cout << "Vehicle Program" << endl << endl;
+    
+    cout << "Vehicle:" << endl;
+    cout << "Enter the manufacturer: " << endl;
+    getline(cin, manufacturer);
+    cout << "Enter the year built: " << endl;
+    cin >> yearBuilt;
+    
+    vehicle.Set_Manufacturer(manufacturer);
+    vehicle.Set_YearBuilt(yearBuilt);
+    
+    vehicle.Display_Info();
+}
