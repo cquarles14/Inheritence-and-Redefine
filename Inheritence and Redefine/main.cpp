d615601bce487b613564da31b6b910ddc29a4f8b
cquarles14 <catlynne.quarles@sinclair.edu>
2022-04-25 23:16:38 -0400

    Initial Commit

---
+++ Inheritence and Redefine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Inheritence and Redefine
+//
+//  Created by Catlynne Quarles on 4/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
